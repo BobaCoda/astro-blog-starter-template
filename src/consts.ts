8630045a548bdf4f46ca0709de2de8c1da7ab9aa
BobaCoda <tripgriffin03@gmail.com>
2025-07-07 21:31:38 -0600

    Meme desc Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "John Smith, the Legend Himself";
+export const SITE_DESCRIPTION = "There is only true John Smith, who stands at the pinnacle of all mankind!";
